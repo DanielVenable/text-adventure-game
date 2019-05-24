96c6eb5edd85a47c83c3ae48150a2c798d5aa7bb
DanielVenable <daniel.t.venable@gmail.com>
2019-05-23 19:34:23 -0700

    First room of test game created.

--- test-game.js
+++ test-game.js
@@ -1,10 +1,9 @@
-var test_game = new Map();
-test_game.set("prison cell",
-	{decription: "You wake up in a small prison cell with no windows. \
+var game = [{name: "prison cell",
+	decription: "You wake up in a small prison cell with no windows. \
 	The door is locked, but you see a small key hanging just outside your reach. \
 	The room is empty execpt for a long stick lying on the floor.",
 	objects: ["stick", "key", "door"],
-	adjacent_locations: []});
+	adjacent_locations: []}];
 var all_objects = [
 	{name: "stick",
 	pick_up: function() {inventory.push("stick"); return "You have a stick!";},
@@ -15,5 +14,8 @@ var all_objects = [
 	
 	{name: "key", pick_up: function() {return "You can't reach it."},
 	actions: [["door", function() {find("door").description = "The door is now open";
-		/*add adjacent location*/return "You put the key in the lock, and the door opens.";}
+		find_location("prison cell").adjacent_locations.push("forest");
+		return "You put the key in the lock, and the door opens.";}
+		
+	{name: "door", pick_up: function() {return "You can't pick up a door."}, actions: [];}
 	];
