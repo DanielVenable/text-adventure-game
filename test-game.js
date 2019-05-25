b31b9d39ffe590cd7f91d3431031c056e78f6ec1
DanielVenable <daniel.t.venable@gmail.com>
2019-05-25 16:56:34 -0700

    Test game works. Commands (inventory, use ... on, and pick up ...) work.

--- test-game.js
+++ test-game.js
@@ -1,21 +1,21 @@
-var game = [{name: "prison cell",
-	decription: "You wake up in a small prison cell with no windows. \
-	The door is locked, but you see a small key hanging just outside your reach. \
-	The room is empty execpt for a long stick lying on the floor.",
+var current_room = "prison cell",
+game = [{name: "prison cell",
+	description: "You wake up in a small prison cell with no windows. \
+The door is locked, but you see a small key hanging just outside your reach. \
+The room is empty execpt for a long stick lying on the floor.",
 	objects: ["stick", "key", "door"],
-	adjacent_locations: []}];
-var all_objects = [
-	{name: "stick",
-	pick_up: function() {inventory.push("stick"); return "You have a stick!";},
-	actions: [["key", function() {
-		find("key").pick_up = function() {inventory.push("key"); return "You have a key!";};
-		find(stick).actions.remove_action("key");
-		return "The stick knocks down the key, putting it within reach."}]]},
+	adjacent_locations: []}],
+objects = [
+	{name: "stick", pick_up: function() {inventory.push("stick"); return "You have a stick!";},
+	actions: [{name:"key", func: function() {
+		find(objects, "key").pick_up = function() {inventory.push("key"); return "You have a key!";};
+		remove_action(find(objects, "stick").actions, "key");
+		return "The stick knocks down the key, putting it within reach."}}]},
 	
-	{name: "key", pick_up: function() {return "You can't reach it."},
-	actions: [["door", function() {find("door").description = "The door is now open";
-		find_location("prison cell").adjacent_locations.push("forest");
-		return "You put the key in the lock, and the door opens.";}
+	{name: "key", pick_up: () => "You can't reach it.", actions: [{
+		name:"door", func: function() {find(objects, "door").description = "The door is now open";
+		find(game, "prison cell").adjacent_locations.push("forest");
+		return "You put the key in the lock, and the door opens.";}}]},
 		
-	{name: "door", pick_up: function() {return "You can't pick up a door."}, actions: [];}
+	{name: "door", pick_up: function() {return "You can't pick up a door."}, actions: []}
 	];
