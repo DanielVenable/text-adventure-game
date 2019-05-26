7908c03d606c919012278e7ee2e3ef025b53bb86
DanielVenable <daniel.t.venable@gmail.com>
2019-05-25 18:04:06 -0700

    Can't pick up same object twice

--- test-game.js
+++ test-game.js
@@ -6,20 +6,19 @@ game = [{name: "prison cell",
 	objects: ["stick", "key", "door"],
 	adjacent_locations: []}],
 objects = [
-	{name: "stick", pick_up: function() {inventory.push("stick"); return "You have a stick!";},
+	{name: "stick", pick_up: () => actually_pick_up("stick"),
 	actions: [{name:"key", func: function() {
-		find(objects, "key").pick_up = function() {
-			inventory.push("key"); return "You have a key!";};
-			remove_action(find(objects, "stick").actions, "key");
-			return "The stick knocks down the key, putting it within reach."}}],
-		description: "It is a long wooden stick."},
+		find(objects, "key").pick_up = () => actually_pick_up("key");
+		remove_action(find(objects, "stick").actions, "key");
+		return "The stick knocks down the key, putting it within reach."}}],
+	description: "It is a long wooden stick."},
 	
 	{name: "key", pick_up: () => "You can't reach it.", actions: [{
 		name:"door", func: function() {
-			find(objects, "door").description = "The door is now open";
+			find(objects, "door").description = "The door is now open.";
 			find(game, "prison cell").adjacent_locations.push("forest");
 			return "You put the key in the lock, and the door opens.";}}],
 		description: "It is a small, brass key."},
 		
-	{name: "door", pick_up: function() {return "You can't pick up a door."}, actions: [],
+	{name: "door", pick_up: () => "You can't pick up a door.", actions: [],
 		description: "The large, metal door is locked shut, blocking your way out."}];
