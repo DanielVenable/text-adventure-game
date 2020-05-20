b9f5ddc6637c8ff2544c8cde52a9ddbe15dd68bd
DanielVenable <daniel.t.venable@gmail.com>
2020-05-19 17:38:02 -0700

    edit page added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const mysql = require('mysql');
+const url = require('url');
 const port = 8000;
 
 var sql = mysql.createConnection({
@@ -17,161 +18,155 @@ sql.connect(function(err) {
 });
 
 const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
-	const split_url = req.url.split('?');
-	if (split_url.length == 2 && split_url[0] == "/play") {
-		const split_request = split_url[1].split('&');
-		if (split_request.length == 4) {
-			const command = decodeURIComponent(split_request[0].split('=')[1].replace(/\+/g, " "));
-			const states = toByteArray(split_request[1].split('=')[1]);
-			const locationID = parseInt(split_request[2].split('=')[1]);
-			const inventory = toByteArray(split_request[3].split('=')[1]);
-			if ([command, states, locationID, inventory].every(a => a !== undefined)) {
-				sql.query(`SELECT * FROM locations WHERE ID = ?;`, [locationID], function (err, location) {
-					if (err) throw err;
-					if (location.length == 1) {
-						const show_data = {
-							res:res, game:location[0].game, states:states, location:locationID, inventory:inventory
-						};
-						sql.query(`SELECT * FROM objects WHERE game = ? ORDER BY ID`, [location[0].game],
-						function (err, objects) {
-							if (err) throw err;
-							if (command.startsWith('go to ')) {
-								sql.query(`SELECT ID, description FROM locations WHERE name = ? AND game = ?;`,
-									[command.split(/^go to /)[1], location[0].game],
-								function (err, end_location) {
-									if (err) throw err;
-									if (end_location.length == 1) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	const parsed_url = url.parse(req.url, true);
+	if (parsed_url.pathname == "/play") {
+		const command = parsed_url.query.cmd;
+		const states = toHalfByteArray(parsed_url.query.a);
+		const locationID = parseInt(parsed_url.query.b);
+		const inventory = toHalfByteArray(parsed_url.query.c);
+		if ([command, states, locationID, inventory].every(a => a !== undefined)) {
+			sql.query(`SELECT * FROM locations WHERE ID = ?;`, [locationID], function (err, location) {
+				if (err) throw err;
+				if (location.length == 1) {
+					const show_data = {
+						res:res, game:location[0].game, states:states, location:locationID, inventory:inventory
+					};
+					sql.query(`SELECT * FROM objects WHERE game = ? ORDER BY ID`, [location[0].game],
+					function (err, objects) {
+						if (err) throw err;
+						if (command.startsWith('go to ')) {
+							sql.query(`SELECT ID, description FROM locations WHERE name = ? AND game = ?;`,
+								[command.split(/^go to /)[1], location[0].game],
+							function (err, end_location) {
+								if (err) throw err;
+								if (end_location.length == 1) {
+									sql.query(`
+										SELECT paths.ID, constraints.obj, constraints.state FROM paths
+										LEFT JOIN path_to_constraint ON paths.ID = path_to_constraint.path
+										LEFT JOIN constraints ON constraints.ID = path_to_constraint.constraint_
+										WHERE paths.start = ? AND paths.end = ?
+										ORDER BY paths.ID;`,
+									[location[0].ID, end_location[0].ID],
+									function(err, constraints){
+										if (err) throw err;
+										if (satisfy_constraints(states, constraints, objects)) {
+											show_data.location = end_location[0].ID;
+											show(show_data, end_location[0].description);
+										} else {
+											show(show_data, 'Nothing happens.');
+										}
+									});
+								} else {
+									show(show_data, 'Nothing happens.');
+								}
+							});
+						} else if (command.startsWith('pick up ')) {
+							const name = command.split(/^pick up /)[1];
+							sql.query(`SELECT ID FROM objects WHERE name = ? AND game = ? AND default_location = ?;`,
+								[name, location[0].game, locationID],
+							function (err, obj) {
+								if (err) throw err;
+								if (obj.length == 1) {
+									if (inventory.includes(obj[0].ID)) {
+										show(show_data, "You already have it.");
+									} else {
 										sql.query(`
-											SELECT paths.ID, constraints.obj, constraints.state FROM paths
-											LEFT JOIN path_to_constraint ON paths.ID = path_to_constraint.path
-											LEFT JOIN constraints ON constraints.ID = path_to_constraint.constraint_
-											WHERE paths.start = ? AND paths.end = ?
-											ORDER BY paths.ID;`,
-										[location[0].ID, end_location[0].ID],
+											SELECT grab.ID, grab.success, constraints.obj, constraints.state FROM grab
+											LEFT JOIN grab_to_constraint ON grab.ID = grab_to_constraint.grab
+											LEFT JOIN constraints ON constraints.ID = grab_to_constraint.constraint_
+											WHERE grab.obj = ?
+											ORDER BY grab.ID;`, [obj[0].ID],
 										function(err, constraints){
 											if (err) throw err;
-											if (satisfy_constraints(states, constraints, objects)) {
-												show_data.location = end_location[0].ID;
-												show(show_data, end_location[0].description);
-											} else {
-												show(show_data, 'Nothing happens.');
-											}
-										});
-									} else {
-										show(show_data, 'Nothing happens.');
-									}
-								});
-							} else if (command.startsWith('pick up ')) {
-								const name = command.split(/^pick up /)[1];
-								sql.query(`SELECT ID FROM objects WHERE name = ? AND game = ? AND default_location = ?;`,
-									[name, location[0].game, locationID],
-								function (err, obj) {
-									if (err) throw err;
-									if (obj.length == 1) {
-										if (inventory.includes(obj[0].ID)) {
-											show(show_data, "You already have it.");
-										} else {
-											sql.query(`
-												SELECT grab.ID, grab.success, constraints.obj, constraints.state FROM grab
-												LEFT JOIN grab_to_constraint ON grab.ID = grab_to_constraint.grab
-												LEFT JOIN constraints ON constraints.ID = grab_to_constraint.constraint_
-												WHERE grab.obj = ?
-												ORDER BY grab.ID;`, [obj[0].ID],
-											function(err, constraints){
-												if (err) throw err;
-												const result = satisfy_constraints(states, constraints, objects);
-												if (result) {
-													sql.query(`
-														SELECT effects.obj, effects.state, effects.text FROM grab
-														JOIN grab_to_effect ON grab.ID = grab_to_effect.grab
-														JOIN effects ON effects.ID = grab_to_effect.effect
-														WHERE grab.ID = ?`, [obj[0].ID],
-													function (err, effects) {
-														if (err) throw err;
-														const text = handle_effects(effects, objects, states);
-														if (result.success) {
-															inventory.push(obj[0].ID);
-															show(show_data, `${text ? text + ' ' : ''}You have ${a_an(name)}.`);
-														} else {
-															show(show_data, text ? text : "Nothing happens.");
-														}
-													});
-												} else {
-													show(show_data, "Nothing happens.");
-												}
-											});
-										}
-									} else {
-										show(show_data, `There is no ${name} here.`);
-									}
-								});
-							} else if (/^use .+/.test(command)) {
-								const objs = command.split(/^use /)[1].split(' on ');
-								if (objs[1] === undefined) {
-									use_on(null, objs[0]);
-								} else {
-									sql.query(`SELECT ID FROM objects WHERE name = ?`, [objs[0]], function (err, item1) {
-										if (err) throw err;
-										if (item1.length == 1 && inventory.includes(item1[0].ID)) {
-											use_on(item1[0].ID, objs[1]);
-										} else {
-											show(show_data, `You don't have ${a_an(objs[0])}`);
-										}
-									});
-								}
-								function use_on(first_ID, second_name) {
-									sql.query(`SELECT ID, default_location FROM objects WHERE name = ?`, [second_name],
-									function (err, item2) {
-										if (err) throw err;
-										var valid_items = [];
-										item2.forEach(function (item) {
-											if (item.default_location == locationID || inventory.includes(item.ID)) {
-												valid_items.push(item);
-											}
-										});
-										if (valid_items.length == 1) {
-											sql.query(`
-												SELECT actions.ID, constraints.obj, constraints.state FROM actions
-												LEFT JOIN action_to_constraint ON actions.ID = action_to_constraint.action
-												LEFT JOIN constraints ON constraints.ID = action_to_constraint.constraint_
-												WHERE actions.obj1 = ? AND actions.obj2 = ?
-												ORDER BY actions.ID;`, [first_ID, item2[0].ID],
-											function(err, constraints){
-												if (err) throw err;
-												const result = satisfy_constraints(states, constraints, objects);
-												if (result) {
-													sql.query(`
-														SELECT effects.obj, effects.state, effects.text FROM actions
-														JOIN action_to_effect ON actions.ID = action_to_effect.action
-														JOIN effects ON effects.ID = action_to_effect.effect
-														WHERE actions.ID = ?;`, [result.ID],
-													function (err, effects) {
-														if (err) throw err;
-														const text = handle_effects(effects, objects, states);
+											const result = satisfy_constraints(states, constraints, objects);
+											if (result) {
+												sql.query(`
+													SELECT effects.obj, effects.state, effects.text FROM grab
+													JOIN grab_to_effect ON grab.ID = grab_to_effect.grab
+													JOIN effects ON effects.ID = grab_to_effect.effect
+													WHERE grab.ID = ?`, [obj[0].ID],
+												function (err, effects) {
+													if (err) throw err;
+													const text = handle_effects(effects, objects, states);
+													if (result.success) {
+														inventory.push(obj[0].ID);
+														show(show_data, `${text ? text + ' ' : ''}You have ${a_an(name)}.`);
+													} else {
 														show(show_data, text ? text : "Nothing happens.");
-													});
-												} else {
-													show(show_data, "Nothing happens.");
-												}
-											});
-										} else {
-											show(show_data, `There is no ${second_name} here`);
+													}
+												});
+											} else {
+												show(show_data, "Nothing happens.");
+											}
+										});
+									}
+								} else {
+									show(show_data, `There is no ${name} here.`);
+								}
+							});
+						} else if (/^use .+/.test(command)) {
+							const objs = command.split(/^use /)[1].split(' on ');
+							if (objs[1] === undefined) {
+								use_on(null, objs[0]);
+							} else {
+								sql.query(`SELECT ID FROM objects WHERE name = ?`, [objs[0]], function (err, item1) {
+									if (err) throw err;
+									if (item1.length == 1 && inventory.includes(item1[0].ID)) {
+										use_on(item1[0].ID, objs[1]);
+									} else {
+										show(show_data, `You don't have ${a_an(objs[0])}`);
+									}
+								});
+							}
+							function use_on(first_ID, second_name) {
+								sql.query(`SELECT ID, default_location FROM objects WHERE name = ?`, [second_name],
+								function (err, item2) {
+									if (err) throw err;
+									var valid_items = [];
+									item2.forEach(function (item) {
+										if (item.default_location == locationID || inventory.includes(item.ID)) {
+											valid_items.push(item);
 										}
 									});
-								}
-							} else {
-								show(show_data, "Invalid command");
+									if (valid_items.length == 1) {
+										sql.query(`
+											SELECT actions.ID, constraints.obj, constraints.state FROM actions
+											LEFT JOIN action_to_constraint ON actions.ID = action_to_constraint.action
+											LEFT JOIN constraints ON constraints.ID = action_to_constraint.constraint_
+											WHERE actions.obj1 = ? AND actions.obj2 = ?
+											ORDER BY actions.ID;`, [first_ID, item2[0].ID],
+										function(err, constraints){
+											if (err) throw err;
+											const result = satisfy_constraints(states, constraints, objects);
+											if (result) {
+												sql.query(`
+													SELECT effects.obj, effects.state, effects.text FROM actions
+													JOIN action_to_effect ON actions.ID = action_to_effect.action
+													JOIN effects ON effects.ID = action_to_effect.effect
+													WHERE actions.ID = ?;`, [result.ID],
+												function (err, effects) {
+													if (err) throw err;
+													const text = handle_effects(effects, objects, states);
+													show(show_data, text ? text : "Nothing happens.");
+												});
+											} else {
+												show(show_data, "Nothing happens.");
+											}
+										});
+									} else {
+										show(show_data, `There is no ${second_name} here`);
+									}
+								});
 							}
-						});
-					} else {
-						invalid_request(res);
-					}
-				});
-			} else {
-				invalid_request(res);
-			}
+						} else {
+							show(show_data, "Invalid command");
+						}
+					});
+				} else {
+					invalid_request(res);
+				}
+			});
 		} else {
 			invalid_request(res);
 		}
@@ -180,15 +175,17 @@ const server = http.createServer((req, res) => {
 			if (err) throw err;
 			var game_list = "";
 			result.forEach(function (game) {
-				game_list += `<li><a href="/start/${encodeURIComponent(game.name)}">${sanitize(game.name)}</a></li>`;
+				game_list += `<li><a href="/start?game=${encodeURIComponent(game.name)}">${sanitize(game.name)}</a></li>`;
 			});
 			res.end(`
 				<html><head><title>Text adventure game</title></head>
-				<body><h3>Choose a game to play:</h3><ul>${game_list}</ul></body></html>
+				<body><h3>Choose a game to play:</h3><ul>${game_list}</ul>
+				<h3>Or <a href="/edit">edit a game</a>.</h3>
+				<h3>Or <a href="/new">make your own game</h3></body></html>
 			`);
 		});
-	} else if (req.url.startsWith('/start/')) {
-		const name = decodeURIComponent(req.url.split(/^\/start\//)[1]);
+	} else if (parsed_url.pathname == '/start') {
+		const name = parsed_url.query.game;
 		sql.query(`SELECT locations.ID, locations.description FROM games JOIN locations ON locations.ID = games.start WHERE games.name = ?`,
 		[name], function (err, result) {
 			if (err) throw err;
@@ -205,9 +202,103 @@ const server = http.createServer((req, res) => {
 				`);
 			}
 		});
+	} else if (req.url == '/edit') {
+		sql.query(`SELECT name, ID FROM games ORDER BY name;`, function (err, result) {
+			if (err) throw err;
+			var game_list = "";
+			result.forEach(game => game_list += `<option>${sanitize(game.name)}</option>`);
+			res.end(`
+				<html><head><title>text adventure game</title></head>
+				<body><form>
+					<label>Choose a game to edit: <select name="game">${game_list}</select></label><br>
+					<input type="submit">
+				</form></body></html>
+			`);
+		});
+	} else if (parsed_url.pathname == '/edit' && parsed_url.query.game) {
+		sql.query(`SELECT * FROM games WHERE name = ?`,	[parsed_url.query.game], (err, game) => {
+			if (err) throw err;
+			if (game.length == 1) {
+				sql.query(`SELECT * FROM locations WHERE game = ?`, [game[0].ID], (err, locations) => {
+					if (err) throw err;
+					var location_list = "";
+					locations.forEach(location => location_list += `
+						<li id="${location.ID}">
+							<span onclick="expand(this.parentElement)">${sanitize(location.name)}</span>
+						</li>
+					`);
+					res.end(`
+						<html><head><title>text adventure game</title><style>
+						li {
+							cursor:pointer;
+							text-decoration:underline;
+						}
+						</style></head><body>
+						<p>Locations</p>
+						<ul class="location">${location_list}</ul>
+						</body><script>
+						const game = ${game[0].ID};
+						var opened = new Set();
+						function expand(element) {
+							if (opened.has(element)) {
+								element.innerHTML = element.innerHTML.split("<ul")[0];
+								opened.delete(element);
+							} else {
+								opened.add(element);
+								const xhttp = new XMLHttpRequest();
+								xhttp.onreadystatechange = function() {
+									if (this.readyState == 4 && this.status == 200)
+										element.innerHTML += this.responseText;
+								}	
+								xhttp.open("GET", 
+									"/expand?game=" + encodeURIComponent(game) + 
+									"&type=" + element.parentElement.className +
+									"&id=" + element.id, true);
+								xhttp.send();
+							}
+						}
+						function save(element) {
+							
+						}
+						</script></html>
+					`);
+				});
+			} else {
+				invalid_request(res);
+			}
+		});
+	} else if (req.url == '/new') {
+		res.end(`
+			<html><head><title>text adventure game</title></head>
+			<body><form method="POST" action="/edit"
+				<p>Choose a name for your game:</p><input name="name">
+				<input type="submit">
+			</form></body></html>
+		`);
+	} else if (parsed_url.pathname == '/expand') {
+		switch (parsed_url.query.type) {
+			case "location":
+				sql.query(`SELECT name, ID FROM objects WHERE default_location = ?`,
+				[parsed_url.query.id], (err, result) => {
+					if (err) throw err;
+					res.write(`<ul class="object">`);
+					for (const obj of result) {
+						res.write(`
+							<li id="${obj.ID}">
+								<span onclick="expand(this.parentElement)">${sanitize(obj.name)}</span>
+							</li>`);
+					}
+					res.end(`</ul>`);
+				});
+				break;
+		
+			default:
+				invalid_request(res);
+		} 
 	} else {
-		res.writeHead(404, {'location': '/404'});
-		res.end(`Sorry, but this page does not exist. Click <a href="/">here</a> to go home.`);	
+		res.writeHead(404, {'content-type': 'text/html'});
+		res.end(`<html><head><title>404</title></head>
+			<body><p>Sorry, but this page does not exist. Click <a href="/">here</a> to go home.</p></body></html>`);	
 	}
 });
 
@@ -277,7 +368,7 @@ function show(data, text) {
 }
 
 function invalid_request(res) {
-	res.statusCode = 400;
+	res.writeHead(404);
 	res.end(`
 		<html><head><title>Oops</title></head>
 		<body><p>Sorry, but the request is invalid. Click <a href="/">here</a> to go home.</p></body></html>
@@ -296,7 +387,7 @@ function toHexString(halfByteArray) {
 		return ('0' + (byte & 0xF).toString(16)).slice(-1);
 	}).join('');
 }
-function toByteArray(hexString) {
+function toHalfByteArray(hexString) {
 	if (typeof hexString == 'string') {
 		var result = [];
 		for (var i = 0; i < hexString.length; i += 1) {
